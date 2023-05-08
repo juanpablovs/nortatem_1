174aa5403761577983178da8bab98c9517a52eb0
juanpablovs <jpvazquez.mba2004@alumni.ie.edu>
2023-05-08 15:57:07 +0200

    removed one function in ceos.js

--- nrt/static/public/js/ceos.js
+++ nrt/static/public/js/ceos.js
@@ -6,17 +6,12 @@ function recordEvent(eventName) {
     // generate event
 }
 
-function printAnswer(id, answer) {
-    typewriterEffect(id, answer);
-}
-
-
 for (let i = 0; i < ceoButtons.length; i++) {
     ceoButtons[i].addEventListener('click', () => {
-		recordEvent(ceoButtons[i].getAttribute("data-id"));
-		printAnswer(
+		typewriterEffect(
 			ceoButtons[i].getAttribute("data-id"),
-			ceoButtons[i].getAttribute("data-answer"),
+			ceoButtons[i].getAttribute("data-answer")
 		);
+		recordEvent(ceoButtons[i].getAttribute("data-id"));
 	});
 }
