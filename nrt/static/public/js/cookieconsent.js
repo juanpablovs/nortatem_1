ac52de098d101c7a132e0f41f798a32934f66e0f
juanpablovs <jpvazquez.mba2004@alumni.ie.edu>
2023-05-08 11:09:19 +0200

    removed loader in js

--- nrt/static/public/js/cookieconsent.js
+++ nrt/static/public/js/cookieconsent.js
@@ -1,24 +1,23 @@
 "use strict";
 
-window.addEventListener("load", function () {
-	const cookieValue = document.cookie.search("nortatem_cookie_status");
+const cookieValue = document.cookie.search("nortatem_cookie_status");
 
-	if (cookieValue === -1) {
-		document.getElementById("cookieconsent").style.display = "block";
+if (cookieValue === -1) {
+	document.getElementById("cookieconsent").style.display = "block";
 
-		const cookieConsentButtonAccept = document.getElementById(
-			"cookieconsentbuttonaccept"
-		);
+	const cookieConsentButtonAccept = document.getElementById(
+		"cookieconsentbuttonaccept"
+	);
 
-		cookieConsentButtonAccept.addEventListener(
-			"click",
-			() => {
-				document.cookie = "nortatem_cookie_status=true;max-age=31536000;path=/";
-				document.getElementById("cookieconsent").style.display = "none";
-			},
-			{
-				once: true,
-			}
-		);
-	}
-});
+	cookieConsentButtonAccept.addEventListener(
+		"click",
+		() => {
+			document.cookie =
+				"nortatem_cookie_status=true;max-age=31536000;path=/";
+			document.getElementById("cookieconsent").style.display = "none";
+		},
+		{
+			once: true,
+		}
+	);
+}
